aff6f63ccf4a953f463ee8692fabbf0b360a4cfb
Sarah Thrasher <sarah.thrasher@acquia.com>
2018-03-12 14:00:31 -0400

    Most stuff working.

--- src/Generators/CogThemeGenerator.php
+++ src/Generators/CogThemeGenerator.php
@@ -18,6 +18,8 @@ class CogThemeGenerator extends BaseGenerator
   protected $templatePath = __DIR__ . "/../../templates";
   protected $destination = 'themes';
 
+
+
   protected function interact(InputInterface $input, OutputInterface $output) {
     $questions['name'] = new Question('Theme name');
     $questions['machine_name'] = new Question('Theme machine name');
@@ -101,15 +103,137 @@ class CogThemeGenerator extends BaseGenerator
         ->content('');
     }
 
+    $this->addFile()
+      ->path($location . '{machine_name}/package.json')
+      ->template('starterkit/package.twig');
+
+    $this->addFile()
+      ->path($location . '{machine_name}/.eslintignore')
+      ->template('starterkit/.eslintignore');
+
+    $this->addFile()
+      ->path($location . '{machine_name}/.stylelintrc.json')
+      ->template('starterkit/.stylelintrc.json');
+
+    $this->addFile()
+      ->path($location . '{machine_name}/logo.png')
+      ->template('starterkit/logo.png');
+
+    $this->addFile()
+      ->path($location . '{machine_name}/install-node.sh')
+      ->template('starterkit/install-node.sh');
+
+    $this->addFile()
+      ->path($location . '{machine_name}/screenshot.png')
+      ->template('starterkit/screenshot.png');
+
+    $dir    = $this->templatePath . '/starterkit/_readme';
+    $files = array_diff(scandir($dir), array('..', '.'));
+    foreach ($files as $file) {
+      $output->writeln($file);
+
+      $this->addFile()
+        ->path($location . '{machine_name}/_readme/' . $file)
+        ->template('starterkit/_readme/' . $file);
+    }
+
+    // @TODO need panel_layouts (layouts)
+    $dir    = $this->templatePath . '/starterkit/panel_layouts';
+    $files = array_diff(scandir($dir), array('..', '.'));
+    foreach ($files as $file) {
+      $output->writeln($file);
+
+      $this->addFile()
+        ->path($location . '{machine_name}/panel_layouts/' . $file)
+        ->template('starterkit/panel_layouts/' . $file);
+    }
+
+    // @TODO need _theming-guide
+    $dir    = $this->templatePath . '/starterkit/_theming-guide';
+    $files = array_diff(scandir($dir), array('..', '.'));
+    foreach ($files as $file) {
+      $output->writeln($file);
+
+      $this->addFile()
+        ->path($location . '{machine_name}/_theming-guide/' . $file)
+        ->template('starterkit/_theming-guide/' . $file);
+    }
+
     // Additional files.
     $option_questions['gulp_tasks'] = new ConfirmationQuestion('Would you like to create gulp files?', TRUE);
 
     $options = $this->collectVars($input, $output, $option_questions);
 
     if ($options['gulp_tasks']) {
+
       $this->addFile()
         ->path($location . '{machine_name}/gulpfile.js')
         ->template('optional/gulpfile.twig');
+
+      // @TODO This seems dense.
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/browser-sync.js')
+        ->template('optional/gulp-tasks/browser-sync.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/build.js')
+        ->template('optional/gulp-tasks/build.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/clean.js')
+        ->template('optional/gulp-tasks/clean.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/clean-css.js')
+        ->template('optional/gulp-tasks/clean-css.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/clean-styleguide.js')
+        ->template('optional/gulp-tasks/clean-styleguide.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/compile-js.js')
+        ->template('optional/gulp-tasks/compile-js.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/compile-sass.js')
+        ->template('optional/gulp-tasks/compile-sass.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/compile-styleguide.js')
+        ->template('optional/gulp-tasks/compile-styleguide.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/default.js')
+        ->template('optional/gulp-tasks/default.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/lint-css.js')
+        ->template('optional/gulp-tasks/lint-css.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/lint-js.js')
+        ->template('optional/gulp-tasks/lint-js.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/minify-css.js')
+        ->template('optional/gulp-tasks/minify-css.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/pa11y.js')
+        ->template('optional/gulp-tasks/pa11y.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/serve.js')
+        ->template('optional/gulp-tasks/serve.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/test-css.js')
+        ->template('optional/gulp-tasks/test-css.js');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/watch.js')
+        ->template('optional/gulp-tasks/watch.js');
     }
 
   }
