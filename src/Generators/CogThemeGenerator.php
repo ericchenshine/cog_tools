ee33990f0ba73256a92a76f416d80e6d173b8284
Sarah Thrasher <sarah.thrasher@acquia.com>
2018-04-16 15:19:27 -0400

    Update style guide options.

--- src/Generators/CogThemeGenerator.php
+++ src/Generators/CogThemeGenerator.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 
 class CogThemeGenerator extends BaseGenerator
@@ -31,7 +32,10 @@ class CogThemeGenerator extends BaseGenerator
 
     // Additional files.
     $option_questions['gulp_tasks'] = new ConfirmationQuestion('Would you like to create gulp files?', TRUE);
-    $option_questions['layouts'] = new ConfirmationQuestion('Would you like to add layout files?', TRUE);
+    $option_questions['layouts'] = new ConfirmationQuestion('Would you like to add layout files?', FALSE);
+    $option_questions['documentation'] = new ConfirmationQuestion('Would you like to add documentation files?', FALSE);
+    $option_questions['theme_settings'] = new ConfirmationQuestion('Would you like to add starter theme settings files?', FALSE);
+    $option_questions['style_guide'] = new ChoiceQuestion('Would you like to include the style guide?', ['No', 'KSS'], 'No');
 
     $options = $this->collectVars($input, $output, $option_questions);
 
@@ -44,11 +48,14 @@ class CogThemeGenerator extends BaseGenerator
 
     $prefix = $vars['machine_name'] . '/' . $vars['machine_name'];
 
-    // Global stuff.
-
+    // Core cog stuff.
     $this->addFile()
       ->path($location . $prefix . '.info.yml')
       ->template('starterkit/theme-info.twig');
+
+    $this->addFile()
+      ->path($location . '{machine_name}/logo.svg')
+      ->template('starterkit/theme-logo.twig');
 
     $this->addFile()
       ->path($location . $prefix . '.libraries.yml')
@@ -62,32 +69,46 @@ class CogThemeGenerator extends BaseGenerator
       ->path($location . $prefix . '.theme')
       ->template('starterkit/theme.twig');
 
+    // Front end tools.
+    $this->addFile()
+      ->path($location . '{machine_name}/package.json')
+      ->template('starterkit/package.twig');
+
+    $this->addFile()
+      ->path($location . '{machine_name}/.eslintignore')
+      ->template('starterkit/.eslintignore');
+
+    $this->addFile()
+      ->path($location . '{machine_name}/.stylelintrc.json')
+      ->template('starterkit/.stylelintrc.json');
+
+    // Example and starter files.
+    if ($options['theme_settings']) {
+      $this->addFile()
+        ->path($location . '{machine_name}/theme-settings.php')
+        ->template('starterkit/theme-settings-form.twig');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/config/install/{machine_name}.settings.yml')
+        ->template('starterkit/theme-settings-config.twig');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/config/schema/{machine_name}.schema.yml')
+        ->template('starterkit/theme-settings-schema.twig');
+    }
+    // Example js file.
     $this->addFile()
       ->path($location . '{machine_name}/js/' . str_replace('_', '-', $vars['machine_name']) . '.js')
       ->template('starterkit/javascript.twig');
 
-    $this->addFile()
-      ->path($location . '{machine_name}/theme-settings.php')
-      ->template('starterkit/theme-settings-form.twig');
-
-    $this->addFile()
-      ->path($location . '{machine_name}/config/install/{machine_name}.settings.yml')
-      ->template('starterkit/theme-settings-config.twig');
-
-    $this->addFile()
-      ->path($location . '{machine_name}/config/schema/{machine_name}.schema.yml')
-      ->template('starterkit/theme-settings-schema.twig');
-
-    $this->addFile()
-      ->path($location . '{machine_name}/logo.svg')
-      ->template('starterkit/theme-logo.twig');
-
+    // Empty directories.
     $this->addDirectory()
       ->path($location . '{machine_name}/templates');
 
     $this->addDirectory()
       ->path($location . '{machine_name}/images');
 
+    // Blank starter css files.
     $css_files = [
       'base/elements.scss',
       'components/block.scss',
@@ -110,59 +131,115 @@ class CogThemeGenerator extends BaseGenerator
         ->content('');
     }
 
-    $this->addFile()
-      ->path($location . '{machine_name}/package.json')
-      ->template('starterkit/package.twig');
-
-    $this->addFile()
-      ->path($location . '{machine_name}/.eslintignore')
-      ->template('starterkit/.eslintignore');
-
-    $this->addFile()
-      ->path($location . '{machine_name}/.stylelintrc.json')
-      ->template('starterkit/.stylelintrc.json');
-
+    // Node and nvm install script.
     $this->addFile()
       ->path($location . '{machine_name}/install-node.sh')
       ->template('starterkit/install-node.sh');
 
-    $dir    = $this->templatePath . '/starterkit/_readme';
-    $files = array_diff(scandir($dir), array('..', '.'));
-    foreach ($files as $file) {
-      $filename = basename($file, '.twig');
-      $this->addFile()
-        ->path($location . '{machine_name}/_readme/' . $filename . '.md')
-        ->template('starterkit/_readme/' . $file);
+    // Documentation.
+    if ($options['documentation']) {
+      $output->writeln('Adding documentation.');
+
+      $dir    = $this->templatePath . '/starterkit/_readme';
+      $files = array_diff(scandir($dir), array('..', '.'));
+      foreach ($files as $file) {
+        $filename = basename($file, '.twig');
+        $this->addFile()
+          ->path($location . '{machine_name}/_readme/' . $filename . '.md')
+          ->template('starterkit/_readme/' . $file);
+      }
+
+      $dir    = $this->templatePath . '/starterkit/_theming-guide';
+      $files = array_diff(scandir($dir), array('..', '.'));
+      foreach ($files as $file) {
+        $filename = basename($file, '.twig');
+        $this->addFile()
+          ->path($location . '{machine_name}/_theming-guide/' . $filename . '.md')
+          ->template('starterkit/_theming-guide/' . $file);
+      }
     }
 
-    $dir    = $this->templatePath . '/starterkit/_theming-guide';
-    $files = array_diff(scandir($dir), array('..', '.'));
-    foreach ($files as $file) {
-      $filename = basename($file, '.twig');
-      $this->addFile()
-        ->path($location . '{machine_name}/_theming-guide/' . $filename . '.md')
-        ->template('starterkit/_theming-guide/' . $file);
+
+    if ($options['style_guide'] == 'KSS') {
+      $output->writeln('Do style guide!');
     }
 
     // Gulp build tasks.
     if ($options['gulp_tasks']) {
+      $output->writeln('Adding gulp tasks.');
 
       $this->addFile()
         ->path($location . '{machine_name}/gulpfile.js')
         ->template('optional/gulpfile.twig');
 
-      $dir    = $this->templatePath . '/optional/gulp-tasks';
-      $files = array_diff(scandir($dir), array('..', '.'));
+//      $dir    = $this->templatePath . '/optional/gulp-tasks';
+//      $files = array_diff(scandir($dir), array('..', '.'));
+//
+//      foreach ($files as $file) {
+//        $this->addFile()
+//          ->path($location . '{machine_name}/gulp-tasks/' .  $file)
+//          ->template('/optional/gulp-tasks/' . $file);
+//      }
+      // @TODO
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/browser-sync.js')
+        ->template('optional/gulp-tasks/browser-sync.twig');
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/build.js')
+        ->template('optional/gulp-tasks/build.twig');
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/clean.js')
+        ->template('optional/gulp-tasks/clean.twig');
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/clean-css.js')
+        ->template('optional/gulp-tasks/clean-css.twig');
 
-      foreach ($files as $file) {
+      if ($options['style_guide'] == 'KSS') {
         $this->addFile()
-          ->path($location . '{machine_name}/gulp-tasks/' .  $file)
-          ->template('/optional/gulp-tasks/' . $file);
+          ->path($location . '{machine_name}/gulp-tasks/clean-styleguide.js')
+          ->template('optional/gulp-tasks/clean-styleguide.twig');
+        $this->addFile()
+          ->path($location . '{machine_name}/gulp-tasks/compile-styleguide.js')
+          ->template('optional/gulp-tasks/compile-styleguide.twig');
       }
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/compile-js.js')
+        ->template('optional/gulp-tasks/compile-js.twig');
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/compile-sass.js')
+        ->template('optional/gulp-tasks/compile-sass.twig');
+
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/default.js')
+        ->template('optional/gulp-tasks/default.twig');
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/lint-css.js')
+        ->template('optional/gulp-tasks/lint-css.twig');
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/lint-js.js')
+        ->template('optional/gulp-tasks/lint-js.twig');
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/minify-css.js')
+        ->template('optional/gulp-tasks/minify-css.twig');
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/pa11y.js')
+        ->template('optional/gulp-tasks/pa11y.twig');
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/serve.js')
+        ->template('optional/gulp-tasks/serve.twig');
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/test-css.js')
+        ->template('optional/gulp-tasks/test-css.twig');
+      $this->addFile()
+        ->path($location . '{machine_name}/gulp-tasks/watch.js')
+        ->template('optional/gulp-tasks/watch.twig');
     }
 
     // Layouts.
     if ($options['layouts']) {
+      $output->writeln('Adding layout files.');
+
       $this->addFile()
         ->path($location . $prefix . '.layouts.yml')
         ->template('optional/layouts.twig');
@@ -184,10 +261,6 @@ class CogThemeGenerator extends BaseGenerator
         }
       }
     }
-
-    // Documentation.
-
-    // KSS Style guide.
 
   }
 
